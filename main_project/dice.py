549aeaeca6569ebdf249621dc76296455fc6e6b0
#222 <nurasel72@gmail.com>
2026-01-19 21:28:20 +0500

    Refactor dice rolling logic with input validation

--- main_project/dice.py
+++ main_project/dice.py
@@ -1,35 +1,43 @@
 import random
 
-dices_art = {
-1: ("┌───────┐", 
-    "│   ●   │", 
-    "│       │",
-    "└───────┘"), 
-2: ("┌───────┐", 
-    "│   ●   │", 
-    "│   ●   │",
-    "└───────┘"),
-3: ("┌───────┐", 
-    "│ ●   ● │", 
-    "│   ●   │",
-    "└───────┘"),
-4: ("┌───────┐", 
-    "│ ●   ● │", 
-    "│ ●   ● │",
-    "└───────┘")
-}
+while True:
+    dices_art = {
+    1: ("┌───────┐", 
+        "│   ●   │", 
+        "│       │",
+        "└───────┘"), 
+    2: ("┌───────┐", 
+        "│   ●   │", 
+        "│   ●   │",
+        "└───────┘"),
+    3: ("┌───────┐", 
+        "│ ●   ● │", 
+        "│   ●   │",
+        "└───────┘"),
+    4: ("┌───────┐", 
+        "│ ●   ● │", 
+        "│ ●   ● │",
+        "└───────┘")
+    }
 
-dice = []
-total = 0
-nmb_dices = int(input("How many dices? "))
+    dice = []
+    total = 0
+    nmb_dices = int(input("How many dices? "))
 
-for die in range(nmb_dices):
-    dice.append(random.randint(1, 4))
+    if nmb_dices == "":
+        print("Write numbers.")
+        nmb_dices = int(input("How many dices? "))
+    elif nmb_dices < 0:
+        print("You can't write negative numbers")
+        nmb_dices = int(input("How many dices? "))
 
-for die in range(nmb_dices):
-    for line in dices_art.get(dice[die]):
-        print(line)
+    for die in range(nmb_dices):
+        dice.append(random.randint(1, 4))
 
-for die in dice:
-    total += die
-print(f"Total: {total}")
+    for die in range(nmb_dices):
+        for line in dices_art.get(dice[die]):
+            print(line)
+
+    for die in dice:
+        total += die
+    print(f"Total: {total}")
