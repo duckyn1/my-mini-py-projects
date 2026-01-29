1ecd087309bad00ba9989e2f57e12539f54b23ef
#222 <nurasel72@gmail.com>
2026-01-29 19:24:21 +0500

    Refactor story generation logic in strygame.py
    Removed story generation delay and adjusted print statements.

--- main_project/strygame.py
+++ main_project/strygame.py
@@ -6,14 +6,9 @@ name2 = input("Write a second name thats spinning in ur head: ")
 topic = input("Write down any topic: ")
 noun = input("Write here what u love: ")
 
-print('Generating story.')
-time.sleep(0.3)
-print('Generating story..')
-time.sleep(0.3)
-print('Generating story...')
-
 print(f"I saw {name} in toilet, and {name2}")
 time.sleep(1)
 print(f"Sorry, i can't discuss abt {topic}")
 time.sleep(1)
 print(f"I love {noun}")
+
